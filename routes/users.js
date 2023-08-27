b02a0b20a72a2aa402389d8b709bc3ee95198a6b
naim-2 <121217709+naim-2@users.noreply.github.com>
2023-08-28 02:48:18 +0300

    Updated the GET functions

--- routes/users.js
+++ routes/users.js
@@ -36,6 +36,27 @@ router.get("/:email",(req,res)=>{
   res.send(filtered_users);
 });
 
+// GET by specific lastName: Retrieve users with the same lastName
+router.get("/lastName/:lastName",(req,res)=>{
+    const lastName = req.params.lastName;
+    let filtered_users = users.filter((user) => user.lastName === lastName);
+    res.send(filtered_users);
+  });
+
+function getDateFromString(strDate) {
+    let [dd,mm,yyyy] = strDate.split('-')
+    return new Date(yyyy+"/"+mm+"/"+dd);
+}
+
+// GET sorted users by DOB
+router.get("/sort",(req,res)=>{
+    let sorted_users=users.sort(function(a, b) {
+        let d1 = getDateFromString(a.DOB);
+        let d2 = getDateFromString(b.DOB);
+            return d1-d2;
+          });
+    res.send(sorted_users);
+});
 
 // POST request: Create a new user
 router.post("/",(req,res)=>{
